815b55cd50c8884bfa4780d3654eaf1278f4d185
sogdiana56-2 <sogdiana0608@icloud.com>
2025-09-24 18:50:41 +0600

    добавила хомворк5

---
+++ HomeWorks/distance.py
@@ -0,0 +1,41 @@
+class Distance:
+    _conversions = {
+        "mm": 0.001,
+        "cm": 0.01,
+        "m": 1,
+        "km": 1000,
+    }
+
+    def __init__(self, value, unit):
+        self.value = value
+        self.unit = unit
+        if unit not in self._conversions:
+            raise ValueError(f"Неподдерживаемая единица: {unit}")
+
+
+    def __str__(self):
+      return f"эксземпляр Distance: {self.value}{self.unit}"
+
+    def convert(self, to_unit="m"):
+        """Конвертируем в любую единицу"""
+        if to_unit not in self._conversions:
+            raise ValueError(f"Неподдерживаемая единица: {to_unit}")
+        meters = self.value * self._conversions[self.unit]
+        return meters / self._conversions[to_unit]
+
+    def __add__(self, other):
+        total = self.convert("m") + other.convert("m")
+        return Distance(total / self._conversions[self.unit], self.unit)
+
+    def __sub__(self, other):
+        total = self.convert("m") - other.convert("m")
+        return Distance(total / self._conversions[self.unit], self.unit)
+
+    def __eq__(self, other):
+        return self.convert("m") == other.convert("m")
+
+
+    def __lt__(self, other):
+        return self.convert("m") < other.convert("m")
+
+
